4c0b432fb22a7228e32f502187f9d8c0a8921741
David Marcec <dmarcecguzman@gmail.com>
2018-02-16 17:11:45 -0800

    code format update

--- KissAnime/desktop.py
+++ KissAnime/desktop.py
@@ -9,26 +9,29 @@ from lxml.cssselect import CSSSelector
 
 BASE_URL = "http://kissanime.ru/"
 MOBILE_BASE_URL = 'http://kissanime.ru/Mobile/'
-USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/56.0"
+USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 \
+Firefox/56.0"
 # Mobile user agent is required to get around the captcha
-MOBILE_USERAGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_1 like Mac OS X)\
- AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A402 Safari/604.1"
+MOBILE_USERAGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_1 like Mac OS X) \
+AppleWebKit/604.1.38 (KHTML, like Gecko) \
+Version/11.0 Mobile/15A402 Safari/604.1"
+
 
 class ConnectionHandler(object):
     """Kissanime Desktop handler"""
     def __init__(self, database='cloudflare.db'):
         self.is_initalized = False
-        self.cf_db = sqlite3.connect(database) # Create/Load DB on disk
-        if not self.initalize_sqldb(): # Initalize if not already created
+        self.cf_db = sqlite3.connect(database)  # Create/Load DB on disk
+        if not self.initalize_sqldb():  # Initalize if not already created
             return
-        self.scrape = self.setup_cloudflare() # Main setup here
-        self.mobile_scrape = self.setup_mobilecloudflare() # Captcha bypass
+        self.scrape = self.setup_cloudflare()  # Main setup here
+        self.mobile_scrape = self.setup_mobilecloudflare()  # Captcha bypass
         self.is_initalized = True
 
     def __del__(self):
         """Free up any objects which need to be closed"""
-        self.is_initalized = False # Not needed but support can be added
-        if self.cf_db: # Close db on cleanup
+        self.is_initalized = False  # Not needed but support can be added
+        if self.cf_db:  # Close db on cleanup
             self.cf_db.close()
 
     def initalize_sqldb(self):
@@ -51,11 +54,13 @@ class ConnectionHandler(object):
         session = requests.Session()
         session.headers.update({'User-Agent': user_agent})
         self.cursor.execute('''SELECT * FROM Cookies
-        WHERE DomainID=''' + str(domain_id)) # DomainID 0 = Desktop, 1 = Mobile
+        WHERE DomainID=''' + str(domain_id))  # DomainID 0=Desktop, 1=Mobile
         sql_row = self.cursor.fetchone()
-        if sql_row is None: # There actually isn't any cookies to check
+        if sql_row is None:  # There actually isn't any cookies to check
             scrape = cfscrape.create_scraper(sess=session)
-            tkn, agt = scrape.get_cookie_string(BASE_URL, user_agent=user_agent)
+            tkn, agt = scrape.get_cookie_string(
+                BASE_URL, user_agent=user_agent
+            )
             self.cursor.execute('''INSERT INTO Cookies(DomainID, Cookies, UserAgent)
             VALUES(?, ?, ?);''', (domain_id, tkn, agt))
             self.cf_db.commit()
@@ -67,18 +72,23 @@ class ConnectionHandler(object):
             req = session.get(BASE_URL)
             if req.status_code == 200: # Valid cookie still
                 return cfscrape.create_scraper(sess=session)
-            else: # Cookie needs to be updated
+            else:  # Cookie needs to be updated
                 session.headers.update({
                     'Cookie': '',
-                }) # Clear previous cookie
+                })  # Clear previous cookie
                 scrape = cfscrape.create_scraper(sess=session)
-                tkn, agt = scrape.get_cookie_string(BASE_URL, user_agent=user_agent)
+                tkn, agt = scrape.get_cookie_string(
+                    BASE_URL, user_agent=user_agent
+                )
                 self.cursor.execute('''UPDATE Cookies
-                SET Cookies=?, UserAgent=? WHERE DomainID=?''', (tkn, agt, domain_id))
+                SET Cookies=?, UserAgent=? WHERE DomainID=?''', (
+                    tkn, agt, domain_id
+                ))
                 session.headers.update({
                     'Cookie': tkn
                 })
                 return scrape
+
 
 class AnimeMetaObject(object):
     """Basic anime information meta object"""
@@ -122,11 +132,12 @@ class AnimeMetaObject(object):
         return self.dictionary()[key.lower()]
 
     def __str__(self):
-        cur_str = 'Title:       %s\n'%self.title
-        cur_str += 'Tags:        %s\n'%', '.join(self.tags)
-        cur_str += 'URL:         %s\n'%self.url
-        cur_str += 'Description: %s'%self.description
+        cur_str = 'Title:       %s\n' % self.title
+        cur_str += 'Tags:        %s\n' % ', '.join(self.tags)
+        cur_str += 'URL:         %s\n' % self.url
+        cur_str += 'Description: %s' % self.description
         return cur_str
+
 
 class AnimeSearchObject(object):
     """Anime meta data for search results"""
@@ -156,9 +167,10 @@ class AnimeSearchObject(object):
         return self.dictionary()[key.lower()]
 
     def __str__(self):
-        cur_str = 'Title:       %s\n'%self.title
-        cur_str += 'URL:         %s'%self.url
+        cur_str = 'Title:       %s\n' % self.title
+        cur_str += 'URL:         %s' % self.url
         return cur_str
+
 
 class AnimeListObject(object):
     """Anime meta data for the anime list"""
@@ -200,6 +212,7 @@ class AnimeListObject(object):
         cur_str += 'URL:         %s'%self.url
         return cur_str
 
+
 class AnimeEpisodeInfoObject(object):
     """Anime meta data for the anime episodes"""
     def __init__(self, title='N/A', url='N/A', date='N/A'):
@@ -239,6 +252,7 @@ class AnimeEpisodeInfoObject(object):
         cur_str += 'Date:        %s\n'%self.date
         cur_str += 'URL:         %s'%self.url
         return cur_str
+
 
 class AnimeEpisodeMetaObject(object):
     """Anime meta data"""
@@ -324,10 +338,11 @@ class AnimeEpisodeMetaObject(object):
         cur_str = cur_str[:-1]
         return cur_str
 
+
 class KissAnime(object):
     """Main API Object"""
     def __init__(self):
-        self.conn = ConnectionHandler() # Initalize connection handler
+        self.conn = ConnectionHandler()  # Initalize connection handler
         assert self.conn.is_initalized
 
     def get_random_anime(self, genre='All', excluded_anime=''):
@@ -336,24 +351,26 @@ class KissAnime(object):
             'selectGenre': genre,
             'excludedAnime': excluded_anime
         })
-        content = self.conn.scrape.post('%sGetRandomAnime?%s'%(BASE_URL, params))
+        content = self.conn.scrape.post('%sGetRandomAnime?%s' % (
+            BASE_URL, params
+        ))
         tree = lxml.html.fromstring(content.text)
         paragraphs = CSSSelector('p')(tree)
         if paragraphs is None or len(paragraphs) < 2:
             return None
-        bigchar = CSSSelector('.bigChar')(tree) # Holds title and URL
+        bigchar = CSSSelector('.bigChar')(tree)  # Holds title and URL
         if bigchar is None or len(bigchar) < 1:
             return None
         bigchar = bigchar[0]
         description = paragraphs[1].get('title')
-        tags = [tag.text for tag in paragraphs[0].cssselect('a')] # Tags are just links
+        tags = [tag.text for tag in paragraphs[0].cssselect('a')]  # Tags a's
         title = bigchar.text
         url = BASE_URL[:-1] + bigchar.get('href')
         return AnimeMetaObject(title, url, tags, description)
 
     def search(self, keyword='', search_type='Anime'):
         """Returns an AnimeSearchObject for a specific search term"""
-        content = self.conn.scrape.post('%sSearch/SearchSuggestx'%BASE_URL, {
+        content = self.conn.scrape.post('%sSearch/SearchSuggestx' % BASE_URL, {
             'type': search_type,
             'keyword': keyword
         })
@@ -362,28 +379,33 @@ class KissAnime(object):
         animes = animes or []
         results = []
         for anime in animes:
-            title = "".join([x for x in anime.itertext()]) # Need to do this due to <span>
-            url = anime.get('href') # All wrapped in <a>
+            title = "".join([x for x in anime.itertext()])  # <span> iterator
+            url = anime.get('href')  # All wrapped in <a>
             results.append(AnimeSearchObject(title, url))
         return results
 
     def get_anime_list(self, letter_filter='', page=0):
         """Returns an AnimeListObject and the max page count with a
         specific filter on a specific page if specified"""
-        if letter_filter == '#': # KissAnime refers to # as 0
+        if letter_filter == '#':  # KissAnime refers to # as 0
             letter_filter = '0'
         params = urllib.urlencode({'c': letter_filter, 'page': page})
-        content = self.conn.scrape.get('%sAnimeList?%s'%(BASE_URL, params))
+        content = self.conn.scrape.get('%sAnimeList?%s' % (BASE_URL, params))
         tree = lxml.html.fromstring(content.text)
-        listing = CSSSelector('.listing tr')(tree) # Get results
+        listing = CSSSelector('.listing tr')(tree)  # Get results
         if len(listing) < 3:
             return []
         results = []
-        for i in range(2, len(listing)): # First two results are garbage and we don't need them
+        for i in range(2, len(listing)):  # First two results are garbage
             anime_listing = listing[i]
-            title = "".join([x.strip() for x in anime_listing[0].itertext()]) # Ignore tags
-            latest_episode = "".join([x.strip() for x in anime_listing[1].itertext()]) # Ignore tags
-            url = BASE_URL[:-1] + anime_listing[0].cssselect('a')[0].get('href') # get anime page
+            title = "".join(
+                [x.strip() for x in anime_listing[0].itertext()]  # Ignore tags
+            )
+            latest_episode = "".join(
+                [x.strip() for x in anime_listing[1].itertext()]  # Ignore tags
+            )
+            url = BASE_URL[:-1]
+            url += anime_listing[0].cssselect('a')[0].get('href')  # anime page
             results.append(AnimeListObject(title, url, latest_episode))
         return results
 
@@ -394,57 +416,61 @@ class KissAnime(object):
             if obj[:1] == '/':
                 url = BASE_URL[:-1] + obj
             else:
-                url = obj # Can take absolute url
+                url = obj  # Can take absolute url
         else:
-            url = obj.get_url() # Any anime meta object
+            url = obj.get_url()  # Any anime meta object
         content = self.conn.scrape.get(url)
         tree = lxml.html.fromstring(content.text)
         listing = CSSSelector('.bigBarContainer')(tree)
         if listing is None or len(listing) < 2:
             return None
-        paragraphs = listing[0].cssselect('p')
-        if paragraphs is None or len(paragraphs) < 5:
+        pgraphs = listing[0].cssselect('p')
+        if pgraphs is None or len(pgraphs) < 5:
             return None
         extra_info = []
-        if len(paragraphs) == 6: # some animes dont have an air date
-            extra_info = [x.strip() for x in paragraphs[3].itertext()]
+        if len(pgraphs) == 6:  # some animes dont have an air date
+            extra_info = [x.strip() for x in pgraphs[3].itertext()]
         else:
-            extra_info = [x.strip() for x in paragraphs[2].itertext()]
-        if len(extra_info) < 5: # Not valid at all?
+            extra_info = [x.strip() for x in pgraphs[2].itertext()]
+        if len(extra_info) < 5:  # Not valid at all?
             return None
         title = listing[0].cssselect('.bigChar')[0]
         if title is None:
             title = 'N/A'
         else:
             title = title.text
-        other_names = [x.text.strip().encode('utf-8') for x in paragraphs[0].cssselect('a')]
-        tags = [x.text.strip().encode('utf-8') for x in paragraphs[1].cssselect('a')]
-        air_date = 'N/A'
-        if len(paragraphs) == 6: # Only if we have 6 <p>'s, airdate is present
-            air_date = "".join([x.strip() for x in paragraphs[2].itertext()])[11:]
+        alt_names = [
+            x.text.strip().encode('utf-8') for x in pgraphs[0].cssselect('a')
+        ]
+        tags = [
+            x.text.strip().encode('utf-8') for x in pgraphs[1].cssselect('a')
+        ]
+        airdate = 'N/A'
+        if len(pgraphs) == 6:  # Only if we have 6 <p>'s, airdate is present
+            airdate = "".join([x.strip() for x in pgraphs[2].itertext()])[11:]
         status = extra_info[2]
         views = extra_info[4]
         summary = 'N/A'
-        if len(paragraphs) == 6: # Due to airdate, summary can be shifted
-            summary = "".join([x.strip() for x in paragraphs[5].itertext()])
+        if len(pgraphs) == 6:  # Due to airdate, summary can be shifted
+            summary = "".join([x.strip() for x in pgraphs[5].itertext()])
         else:
-            summary = "".join([x.strip() for x in paragraphs[4].itertext()])
+            summary = "".join([x.strip() for x in pgraphs[4].itertext()])
 
-        episode_list = listing[1].cssselect('tr')
-        episode_meta = []
+        ep_list = listing[1].cssselect('tr')
+        ep_meta = []
         if len(episode_list) > 2:
-            for i in range(2, len(episode_list)): # First two elements are junk to us
+            for i in range(2, len(episode_list)):  # First two are junk
                 episode = episode_list[i]
                 info = episode.cssselect('td')
                 if len(info) < 2:
                     continue
-                episode_name = "".join([x.strip() for x in info[0].itertext()])
-                episode_url = BASE_URL[:-1] + info[0].cssselect('a')[0].get('href')
-                episode_release = info[1].text.strip() # Release has lots of whitespace
-                episode_meta.append(AnimeEpisodeInfoObject(episode_name,
-                                                           episode_url, episode_release))
-        return AnimeEpisodeMetaObject(title, other_names, tags,
-                                      air_date, status, views, summary, episode_meta)
+                ep_name = "".join([x.strip() for x in info[0].itertext()])
+                ep_url = BASE_URL[:-1]
+                ep_url += info[0].cssselect('a')[0].get('href')
+                ep_rel = info[1].text.strip()  # Remove whitespace
+                ep_meta.append(AnimeEpisodeInfoObject(ep_name, ep_url, ep_rel))
+        return AnimeEpisodeMetaObject(title, alt_names, tags,
+                                      airdate, status, views, summary, ep_meta)
 
     def get_anime_episode(self, obj):
         """Returns video url"""
@@ -453,15 +479,16 @@ class KissAnime(object):
             if obj[:1] == '/':
                 url = BASE_URL[:-1] + obj
             else:
-                url = obj # Can take absolute url
+                url = obj  # Can take absolute url
         else:
-            url = obj.get_url() # Any anime meta object
-        parsed_url = urlparse.urlparse(url)
-        episode_id = urlparse.parse_qs(parsed_url.query)['id'][0] # Grab episode ID from url
-        referer = BASE_URL + 'M' + parsed_url.path[:parsed_url.path.rfind('/')] # Strip episode name
-        content = self.conn.mobile_scrape.post('%sGetEpisode'%MOBILE_BASE_URL, {
-            'eID': episode_id
-        }, headers={
-            'Referer': referer # Needed or the request fails
-        }) # Mobile site required to bypass captcha
-        return content.text.split('|||')[0] # Get video url
+            url = obj.get_url()  # Any anime meta object
+        p_url = urlparse.urlparse(url)
+        episode_id = urlparse.parse_qs(p_url.query)['id'][0]  # episode ID
+        referer = BASE_URL + 'M' + p_url.path[:p_url.path.rfind('/')]  # EpName
+        content = self.conn.mobile_scrape.post(
+            '%sGetEpisode' % MOBILE_BASE_URL, {
+                'eID': episode_id
+            }, headers={
+                'Referer': referer  # Needed or the request fails
+            })  # Mobile site required to bypass captcha
+        return content.text.split('|||')[0]  # Get video url
--- example.py
+++ example.py
@@ -1,19 +1,21 @@
 """EasyKissAnime API Example"""
 from KissAnime import desktop
 
+
 def main():
     """Entry point"""
     kiss_desktop = desktop.KissAnime()
-    anime_search = kiss_desktop.search('Dragonball') # Returns array of search results
+    anime_search = kiss_desktop.search('Dragonball')  # Returns search array
     print 'Dragonball Search Results'
     for search_result in anime_search:
-        print search_result # Print all search info for every result
+        print search_result  # Print all search info for every result
         print '-'*8
     anime_info = kiss_desktop.get_anime_info(anime_search[0])
-    print 'Anime info for "%s"'%anime_search[0].get_title()
+    print 'Anime info for "%s"' % anime_search[0].get_title()
     print anime_info
     print '-'*16
-    episode_url = kiss_desktop.get_anime_episode(anime_info.get_episodes()[0]) # Latest episode
+    # Get the latest episode
+    episode_url = kiss_desktop.get_anime_episode(anime_info.get_episodes()[0])
     print 'Latest episode URL'
     print episode_url
 
